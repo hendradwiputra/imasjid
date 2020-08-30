403ac046df28659853d6fc2f82ad0553232a027d
hendradwiputra <hendra.doank@gmail.com>
2020-08-30 17:25:47 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "monitor";
-	$password = "monitor";
-	$db="iMasjid";
-	// Create connection
-	$conn = mysqli_connect($servername, $username, $password,$db);
-?>
