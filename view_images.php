19e5fc35ea35edf8fc8b72df262570c5372865f7
hendradwiputra <hendra.doank@gmail.com>
2020-09-11 06:51:40 +0700

    Add files via upload

--- view_images.php
+++ view_images.php
@@ -16,7 +16,7 @@
 			echo "<td valign='bottom' colspan='2' class='text-center'><a href='$id'><img src='assets/images/$filename' width='195px' height='130px'></a></td>";
 
 			$count++;
-            if($count >=3){
+            if($count >=4){
                 echo "</tr><tr>";                
                 $count = 0;
             }   
