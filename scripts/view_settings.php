7f685d7d63c62bb4025d0d72718cd1af7a9bd3fa
hendradwiputra <hendra.doank@gmail.com>
2020-09-21 07:28:18 +0700

    Add files via upload

---
+++ scripts/view_settings.php
@@ -0,0 +1,31 @@
+<?php
+  include("db_connection.php");
+  
+	$sql = "SELECT * FROM settings";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      $setting_id = $row['setting_id'];
+      $nama_masjid = $row['nama_masjid'];
+      $alamat_masjid = $row['alamat_masjid'];
+      $kecepatan_teks = $row['kecepatan_teks'];
+      $running_teks = $row['running_teks'];
+      $durasi_slide = $row['durasi_slide'];
+      $koreksi_hijriah = $row['koreksi_hijriah'];
+      $durasi_adzan = $row['durasi_adzan'];
+      $iqomah_shubuh = $row['iqomah_shubuh'];
+      $iqomah_dzuhur = $row['iqomah_dzuhur'];
+      $iqomah_ashar = $row['iqomah_ashar'];
+      $iqomah_maghrib = $row['iqomah_maghrib'];
+      $iqomah_isya = $row['iqomah_isya'];
+      $garis_lintang = $row['garis_lintang'];
+      $garis_bujur = $row['garis_bujur'];
+      $zona_waktu = $row['zona_waktu'];
+      $metode_perhitungan = $row['metode_perhitungan'];
+    }
+  }
+ 
+  $conn->close();
+ 
+?>
