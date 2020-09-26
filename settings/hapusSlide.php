d9806b08bd43280e1f50282ebc95eda821b7df8a
hendradwiputra <hendra.doank@gmail.com>
2020-09-26 07:27:09 +0700

    Add files via upload

--- settings/hapusSlide.php
+++ settings/hapusSlide.php
@@ -4,7 +4,7 @@
   if(isset($_POST["slide_id"])) {  
     $sql = "DELETE FROM slides WHERE slide_id = '".$_POST["slide_id"]."'";
       if ($conn->query($sql) === TRUE) {
-        echo "Data berhasil dihapus";     
+        echo "<script>alert('Data berhasil dihapus.');</script>";     
       }
   } 
 
