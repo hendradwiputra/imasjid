6ce6eed2d7199dba4d6de74f365ff1144489ed95
hendradwiputra <hendra.doank@gmail.com>
2020-11-24 06:42:53 +0700

    update

--- timer.php
+++ timer.php
@@ -38,8 +38,9 @@
 
 <body onload="startTime()">
   <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
-      <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
+  <section id="hero" class="d-flex align-items-center" style="background-image: url(./assets/images/background/timer-bg.jpg)">
+    
+      <div class="container">
         <div class="row justify-content-center">
 
           <div class="col-xl-12 col-lg-12 text-center">
@@ -66,9 +67,9 @@
           </div>
 
         </div>
-
       </div>
-    </section>
+
+  </section>
   <!-- ======= Hero Section ======= -->
 
 </body>
